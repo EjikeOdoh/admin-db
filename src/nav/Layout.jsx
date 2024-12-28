fb2e4ccce80f217b37df2ed7a3228d85bd25a209
EjikeOdoh <ejike.developer@gmail.com>
2024-12-28 10:19:18 +0100

    Fix outlet fill width

--- src/nav/Layout.jsx
+++ src/nav/Layout.jsx
@@ -1,35 +1,54 @@
 import { NavLink, Outlet } from "react-router";
 import './layout.css'
+import { useDispatch } from "react-redux";
 
 export default function Layout() {
-    return <div>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/" end>
-                        Dashboard
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/analytics">Analytics</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/profile-management">Profile Management</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/user-management">User Management</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/settings">Settings</NavLink>
-                </li>
-            </ul>
+    const dispatch = useDispatch()
+
+    function logOut() {
+        
+    }
+
+    return <div className="main">
+            <nav className="py-8 px-4">
+                <div className="flex flex-col gap-4">
+                    
+                        <NavLink className="block" to="/" end>
+                            Dashboard
+                        </NavLink>
+                    
+                    
+                        <NavLink className="block" to="/analytics">Analytics</NavLink>
+                    
+                    
+                        <NavLink className="block" to="/profile-management">Profile Management</NavLink>
+                    
+                    
+                        <NavLink className="block" to="/user-management">User Management</NavLink>
+                    
+                </div>
+
+                <div>
+                    <NavLink className="block mb-4" to="/settings">Settings</NavLink>
+
+                    <button className="block w-full text-left">Log out</button>
+                </div>
+             
+                    
+                       
+                    
+                
 
 
 
-            {/* Mobile Menu */}
-        </nav>
-        <main>
-            <Outlet />
-        </main>
-    </div>
+                {/* Mobile Menu */}
+            </nav>
+            <main className="py-8 px-4">
+                <div className="flex justify-between items-center mb-8">
+                    <h1>Overview</h1>
+                    <p>Interest</p>
+                </div>
+                <Outlet />
+            </main>
+        </div>
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 export default function Dashboard(){
 
-    return <div className=" text-center"> 
+    return <div className="text-center"> 
         <h1>Dashboard</h1>
     </div>
 }
